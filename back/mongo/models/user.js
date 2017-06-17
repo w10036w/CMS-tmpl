9f4e8b0edb2719f3f9f79157994a15f8ee8d1012
sunfangzhou <sunfangzhou@mozat.com>
2017-06-18 05:14:40 +0800

    use crypto instead of bcrypt (unstable nodejs version)

--- back/mongo/models/user.js
+++ back/mongo/models/user.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
-const bcrypt = require('bcrypt')
-const SALT = 10
+const crypto = require('crypto')
+// only stable version supported
+// const bcrypt = require('bcrypt')
+// const SALT = 10
 
 const Schema = mongoose.Schema;
 const ObjectId  = Schema.ObjectId;
@@ -13,6 +15,7 @@ const schema = new Schema(Object.assign({}, {
     index: true, unique: true, required: true },
   displayName: { type: String, default: '', index: true },
   hashed_password: String,
+  salt: String,
   email: String,
   avatar: String,
   role: { type: String, default: 'editor' },
@@ -21,21 +24,52 @@ const schema = new Schema(Object.assign({}, {
 
 schema.virtual('password')
   .set(function (password) {
-    this._password = password
-    log.debug(password)
-    this.hashed_password = this.encryptPassword(password)
+    this._password = password;
+    this.salt = this.makeSalt();
+    this.hashed_password = this.encrypt(password)
   }).get(function () {
   return this._password
 });
-
 schema.methods = {
-  verify (plain) {
-    return bcrypt.compareSync(plain, this.hashed_password)
+  verify (plainText) {
+    const encrypted = this.encrypt(plainText);
+    const hashed = this.hashed_password;
+    console.log(encrypted, hashed)
+    return encrypted === hashed;
   },
-  encryptPassword (password) {
+  encrypt (password) {
     if (!password) return '';
-    return bcrypt.hashSync(password, SALT)
-  }
+    try {
+      return crypto
+        .createHmac('sha256', this.salt + '')
+        .update(password)
+        .digest('hex');
+    } catch (err) {
+      return err;
+    }
+  },
+  makeSalt() {
+    return Math.round((new Date().valueOf() * Math.random()))
+  },
 }
+
+// schema.virtual('password')
+//   .set(function (password) {
+//     this._password = password
+//     log.debug(password)
+//     this.hashed_password = this.encryptPassword(password)
+//   }).get(function () {
+//   return this._password
+// });
+
+// schema.methods = {
+//   verify (plain) {
+//     return bcrypt.compareSync(plain, this.hashed_password)
+//   },
+//   encryptPassword (password) {
+//     if (!password) return '';
+//     return bcrypt.hashSync(password, SALT)
+//   }
+// }
 
 module.exports = mongoose.model('user', schema)
